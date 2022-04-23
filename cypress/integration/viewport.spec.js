b2535e99b2f73cad04ada2ce70f59e429813dcfa
Akalanka Perera <akalanka@parallax.lk>
2022-04-23 21:14:31 +0530

    Minor fix

--- cypress/integration/viewport.spec.js
+++ cypress/integration/viewport.spec.js
@@ -7,33 +7,35 @@ context('Viewport', () => {
 
   it('Check app visibility on different viewports', () => {
 
+    const waitTime = 3000
+
     cy.viewport('macbook-15')
-    cy.wait(200)
+    cy.wait(waitTime)
     cy.viewport('macbook-13')
-    cy.wait(200)
+    cy.wait(waitTime)
     cy.viewport('macbook-11')
-    cy.wait(200)
+    cy.wait(waitTime)
     cy.viewport('ipad-2')
-    cy.wait(200)
+    cy.wait(waitTime)
     cy.viewport('ipad-mini')
-    cy.wait(200)
+    cy.wait(waitTime)
     cy.viewport('iphone-6+')
-    cy.wait(200)
+    cy.wait(waitTime)
     cy.viewport('iphone-6')
-    cy.wait(200)
+    cy.wait(waitTime)
     cy.viewport('iphone-5')
-    cy.wait(200)
+    cy.wait(waitTime)
     cy.viewport('iphone-4')
-    cy.wait(200)
+    cy.wait(waitTime)
     cy.viewport('iphone-3')
-    cy.wait(200)
+    cy.wait(waitTime)
 
     // cy.viewport() accepts an orientation for all presets
     // the default orientation is 'portrait'
     cy.viewport('ipad-2', 'portrait')
-    cy.wait(200)
+    cy.wait(waitTime)
     cy.viewport('iphone-4', 'landscape')
-    cy.wait(200)
+    cy.wait(waitTime)
 
     // The viewport will be reset back to the default dimensions
     // in between tests (the  default can be set in cypress.json)
